688bbe9643be86420dc84ad18a4c3d166abc06e3
Christian Ølund <christian.olund@hotmail.com>
2020-02-17 11:43:57 +0100

    Added chessboard button

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-window.addEventListener("DOMContentLoaded", chess_board);
 
 
-function chess_board(){
-    let stop = 8;
+window.addEventListener("DOMContentLoaded", (e) => {
+    let button = document.getElementById("MakeChessboard");
+    button.addEventListener("click", make_board);
+});
+
+function make_board(){
+    let size = document.getElementById("ChessboardSize");
+    chess_board(size.value);
+}
+
+
+function chess_board(stop){
     let output = "";
     let board = document.getElementById("Chessboard");
-    for(i = 0; i <= stop; i++){
+    for(i = 0; i < stop; i++){
         if(i % 2 == 1){
             output += "█ █ █ █\n";
         }
