8a32b9700401dfa42d57bfe2823afa9d2158e75a
Christian Ølund <christian.olund@hotmail.com>
2020-02-18 08:56:53 +0100

    Big changes

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
+"use strict"
 
+window.addEventListener("DOMContentLoaded", addEventListeners);
 
-window.addEventListener("DOMContentLoaded", (e) => {
+let BalloonSize = 13;
+
+function addEventListeners(){
     let button = document.getElementById("MakeChessboard");
     button.addEventListener("click", make_board);
     let size = document.getElementById("ChessboardSize");
     size.addEventListener("change", make_board);
+    window.addEventListener("keydown", balloon);
+}
+
+function balloon(event){
+    let Balloon = document.getElementById("Balloon");
+    console.log("heY")
+    if(event.key == "ArrowUp"){
+        Balloon.style.fontSize = `${BalloonSize}px`;
+        BalloonSize = Math.floor(BalloonSize * 1.1);
+    }
+    else if(event.key == "ArrowDown"){
+        Balloon.style.fontSize = `${BalloonSize}px`;
+        BalloonSize = Math.floor(BalloonSize * 0.9);
+    }
+    if(BalloonSize > 200){
+        window.removeEventListener("keydown", balloon)
+        Balloon.textContent = "Hej So!" //"💥";
+    }
     
-});
+}
 
 function make_board(){
     let size = document.getElementById("ChessboardSize");
-    chess_board(size.value);
+    if (size == 8){
+        fox_n_hound_game(size.value);
+    }
+    else{
+        chess_board(size.value);
+    }
+}
+
+function fox_n_hound_game(){
+    let fox = document.getElementById("Fox");
+    let hound = document.getElementById("Hound");
 }
 
 
 function chess_board(stop){
     let output = "";
+    let i;
+    let j;
     let board = document.getElementById("Chessboard");
     for(i = 0; i < stop; i++){
         for(j = 0; j < stop; j++){
